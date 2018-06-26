e8789de504858075634469837863862806bdc9d3
LAPTOP-J5SPII67\Acer <vikrantrath4@gmail.com>
2018-06-27 03:07:09 +0530

    v1.1

--- app.js
+++ app.js
@@ -1,4 +1,22 @@
-console.log('Starting App');
+const fs = require('fs');
 const _ = require('lodash');
+const yargs = require('yargs').argv;
+
 notes = require('./notes.js');
-notes.add(2,5);
+
+var command = yargs._[0];
+// console.log(process.argv);
+// console.log(yargs.argv);
+
+if (command === 'add') {
+    notes.addNote(yargs.title,yargs.body)
+} else if (command === 'list') {
+    notes.getAll();
+} else if (command === 'remove') {
+    notes.removeNote(yargs.title);
+} else if (command === 'read') {
+    notes.getNote(yargs.title);
+}
+else {
+    console.log("Command Not Found");
+}
--- notes.js
+++ notes.js
@@ -1,3 +1,23 @@
-module.exports.add = (a,b) =>{
-    console.log(a+b)
+
+var addNote = (title, body) => {
+    console.log("Adding Note with Title",title);
+};
+
+var getAll = () => {
+    console.log("getting all notes");
+};
+
+var removeNote = (title) => {
+    console.log("Removing Note : ",title);
+};
+
+var getNote = (title) => {
+    console.log("Getting a Note", title);
+};
+module.exports = {
+    addNote: addNote, //in es6 only addNote will do since same name
+    getAll,
+    removeNote,
+    getNote,
 }
+
