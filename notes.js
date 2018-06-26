bb872239d04b9f6e2d8164722ad2c51102144a8c
LAPTOP-J5SPII67\Acer <vikrantrath4@gmail.com>
2018-06-27 03:32:34 +0530

    added adding notes functionality

--- notes.js
+++ notes.js
@@ -1,6 +1,25 @@
+const fs = require('fs');
+var notes = [];
 
 var addNote = (title, body) => {
-    console.log("Adding Note with Title",title);
+    //console.log("Adding Note with Title",title);
+    
+    var note = {
+        title,
+        body,
+    }
+    try{
+    noteString = fs.readFileSync('notes-data.json')
+    notes =JSON.parse(noteString);
+    }
+    catch(e){
+
+    }
+    var duplicateNotes = notes.filter((note) => note.title === title);
+    if(duplicateNotes.length === 0){
+    notes.push(note);
+    fs.writeFileSync('notes-data.json',JSON.stringify(notes));
+    }
 };
 
 var getAll = () => {
